7ecf8fdb30b082426837ebc460724dfe698c9410
Neha Binwal <nbinwal2@gmail.com>
2026-01-03 20:00:25 +0530

    Spring Initializr Basic Set-up

---
+++ src/main/java/com/spring_projects/spring_batch_payroll/SpringBatchPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.spring_projects.spring_batch_payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPayrollApplication.class, args);
+	}
+
+}
